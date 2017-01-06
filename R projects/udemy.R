84c7b5bc6fabb711d76c791ef99252a8e93fa7a4
aishsachdeo <aishsachdeo@gmail.com>
2017-01-07 00:04:20 +0800

    delete udemy

--- R projects/udemy.R
+++
@@ -1,6 +0,0 @@
-
-head(select(mtcars, mpg, hp))
-distinct(mtcars, gear)
-head(mutate(mtcars, Performance = hp/wt))
-summarise(mtcars, avg_mpg = mean(mpg))
-mtcars %>% filter(cyl == 6) %>% summarise(std_hp=mean(hp))
